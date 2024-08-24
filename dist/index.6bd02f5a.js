1d877ee5dcf067b23017c8e135eb38a06b86d778
mo-arman <moharman3818@gmail.com>
2024-08-24 20:58:20 +0530

    The

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,23 @@
+// Create nested React Elements
+const heading = React.createElement("h1", {
+    id: "title",
+    style: {
+        background: "red"
+    },
+    className: "title"
+}, "This is Heading");
+const heading1 = React.createElement("h1", {
+    id: "title"
+}, "This is heading1");
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading,
+    heading1
+]);
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(container);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
